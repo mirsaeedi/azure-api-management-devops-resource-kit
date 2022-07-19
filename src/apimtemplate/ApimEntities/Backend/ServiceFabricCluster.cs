841db4ab67e24ebc29b34e60637030f9af56a46d
levimatheri <levimatheri@gmail.com>
2022-07-19 11:55:55 -0700

    Closes #43. Add Service Fabric client certificate id property (#48)
    * Add Service Fabric client certificate id property
    
    * Fix mapping profile for Backends
    
    * add string null/empty check
    
    * nit: reordering mappings
    
    * Fix backend mapping for ServiceFabricCluster certificate id
    
    Co-authored-by: Levi Muriuki <muriukilm@upmc.edu>

--- src/apimtemplate/ApimEntities/Backend/ServiceFabricCluster.cs
+++ src/apimtemplate/ApimEntities/Backend/ServiceFabricCluster.cs
@@ -1,11 +1,12 @@
 ﻿namespace Apim.DevOps.Toolkit.ApimEntities.Backend
 {
-	public class ServiceFabricCluster
-	{
-		public string ClientCertificatethumbprint { get; set; }
-		public int MaxPartitionResolutionRetries { get; set; }
-		public string[] ManagementEndpoints { get; set; }
-		public string[] ServerCertificateThumbprints { get; set; }
-		public ServerX509Names[] ServerX509Names { get; set; }
-	}
+    public class ServiceFabricCluster
+    {
+        public string ClientCertificateId { get; set; }
+        public string ClientCertificatethumbprint { get; set; }
+        public int MaxPartitionResolutionRetries { get; set; }
+        public string[] ManagementEndpoints { get; set; }
+        public string[] ServerCertificateThumbprints { get; set; }
+        public ServerX509Names[] ServerX509Names { get; set; }
+    }
 }
--- src/apimtemplate/Core/Mapping/BackendMapper.cs
+++ src/apimtemplate/Core/Mapping/BackendMapper.cs
@@ -1,14 +1,22 @@
 ﻿using Apim.DevOps.Toolkit.ApimEntities.Backend;
 using Apim.DevOps.Toolkit.Core.DeploymentDefinitions.Entities;
+using Apim.DevOps.Toolkit.Core.Infrastructure.Constants;
 using AutoMapper;
 
 namespace Apim.DevOps.Toolkit.Core.Mapping
 {
-	public class BackendMapper: IMapper
-	{
-		public void Map(IMapperConfigurationExpression cfg)
-		{
-			cfg.CreateMap<BackendDeploymentDefinition, BackendProperties>();
-		}
-	}
+    public class BackendMapper : IMapper
+    {
+        public void Map(IMapperConfigurationExpression cfg)
+        {
+            cfg.CreateMap<BackendDeploymentDefinition, BackendProperties>()
+                .AfterMap((backendDeploymentDefinition, backendProperties) =>
+                {
+                    if (backendDeploymentDefinition?.Properties?.ServiceFabricCluster?.ClientCertificateId is not null)
+                    {
+                        backendProperties.Properties.ServiceFabricCluster.ClientCertificateId = $"[resourceId('{ResourceType.Certificate}', parameters('ApimServiceName'), '{backendDeploymentDefinition.Properties.ServiceFabricCluster.ClientCertificateId}')]";
+                    }
+                });
+        }
+    }
 }
